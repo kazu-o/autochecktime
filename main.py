08151e670462d737fd7b5f1ed5009f654d8fb6b7
kazu-o <k.9k.9.mn721@gmail.com>
2019-01-14 23:08:24 +0900

    勤務表ファイルオープン

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
+import glob
 import xlrd
 
 # 参照情報(フォルダパス、参照期間)
-folderPath = []
-startDay = []
-endDay = []
+folderPaths = []
+startDays = []
+endDays = []
 
 preferenceFile = open('reference.txt', 'r')
 textLines = preferenceFile.readlines()   
 
 # 1つ目の参照情報取得
-folderPath.append(textLines[1])
-startDay.append(textLines[4])
-endDay.append(textLines[5])
+folderPaths.append(textLines[1])
+startDays.append(textLines[4])
+endDays.append(textLines[5])
 
 # 2つ目の参照情報取得
 if not textLines[8]:
-    folderPath.append(textLines[8])
-    startDay.append(textLines[11])
-    endDay.append(textLines[12])
-
-
+    folderPaths.append(textLines[8])
+    startDays.append(textLines[11])
+    endDays.append(textLines[12])
 
 preferenceFile.close
+
+
+# セルの値クリア
+
+# 期間入力
+
+
+# パスから勤務表ファイル名取得
+for folderPath in folderPaths
+    workFilesName = glob.glob(folderPath + '*.xlsx')
+
+    # 勤務表から作業時間取得
+    for workFileName in workFilesName
+        workFile = xlrd.open_workbook(workFileName)
+        workSheet = workFile.sheet_by_name('作業時間')
+
+
+# 名前取得
+    # 名前変換
+# 時間取得
+    # 表記変更
+# 表示
+
+# redmineでも同様
+
+# 一括表示
+# 自動チェックして不一致のみ表示
